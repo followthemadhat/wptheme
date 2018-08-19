9b58c418af27a42a14e1cf2d42b046c8a77561c7
G14 <G14@G14s-iMac.local>
2018-08-20 02:43:49 +0500

    updates

--- customtheme/template-parts/content.php
+++ customtheme/template-parts/content.php
@@ -9,51 +9,16 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				customtheme_posted_on();
-				customtheme_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php customtheme_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'customtheme' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'customtheme' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php customtheme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<div class="post">
+	<div class="post-image" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+		<a href="<?php echo the_permalink(); ?>"></a>
+	</div>
+	<div class="post-content">
+		<a href="<?php echo the_permalink(); ?>"><h4><?php echo the_title(); ?></h4></a>
+		<div class="post-meta">
+			<span class="post-date"><?php echo get_the_date(); ?></span><span class="post-category"><?php the_category(); ?></span>
+		</div>
+		<p><?php echo get_the_excerpt(); ?></p>
+		<a href="<?php echo the_permalink(); ?>" class="post-link button">Read more</a>
+	</div>
+</div>
